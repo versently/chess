030dbafd7c093fed7ec4fcf78d23fdf083223203
Karen.Papikyan <karen.papikyan@c02g50dtmd6r-mbp.home>
2024-09-19 14:01:49 +0300

    add node adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import vercel from "@astrojs/vercel/serverless";
+
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   integrations: [tailwind()],
-  adapter: vercel()
+  adapter: node({
+    mode: "standalone"
+  })
 });
