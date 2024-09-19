d42b4b9b94c3935b7e3f47f48e39931bd73249fa
Karen.Papikyan <karen.papikyan@c02g50dtmd6r-mbp.home>
2024-09-19 14:43:35 +0300

    add netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import vercel from "@astrojs/vercel/serverless";
+
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
   integrations: [tailwind()],
-  adapter: vercel()
+  output: "server",
+  adapter: netlify()
 });
